2702b5db32481bc60f4fafe8ed246a73984f3f3e
nazimie123 <2889710307@qq.com>
2021-04-16 15:50:14 +0800

    第一次提交

---
+++ src/main/java/com/yidu/ssmdemo003/Ssmdemo003Application.java
@@ -0,0 +1,13 @@
+package com.yidu.ssmdemo003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssmdemo003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssmdemo003Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/yidu/ssmdemo003/controller/UserCotroller.java
@@ -0,0 +1,23 @@
+package com.yidu.ssmdemo003.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserCotroller {
+    @RequestMapping("registry")
+    @ResponseBody
+    public  String  registry(String userName,String userPass){
+        System.out.println("userName = " + userName + ", userPass = " + userPass );
+        return "注册成功！ userName = " + userName + ", userPass = " + userPass;
+    }
+
+    @RequestMapping("login")
+    @ResponseBody
+    public  String login(String userName,String userpass){
+        System.out.println("userName = " + userName + ", userpass = " + userpass);
+
+        return "登陆成功！userName = " + userName + ", userpass = " + userpass ;
+    }
+}
